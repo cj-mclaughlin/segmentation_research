381cbe7b81346695341bead588d38c7579879bff
Connor McLaughlin <46900583+cj-mclaughlin@users.noreply.github.com>
2021-07-19 00:47:39 -0400

    fpn/resnet and pip structure (#1)
    * started fpn
    
    * add fpn/resnet and fix project structure

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,11 +1,18 @@
-from segmentation_research.models.pspnet import pspnet
+from segmentation_research.models.pspnet import PSPNet
+from segmentation_research.models.fpn import FPN
 
 def test_pspnet_valid_shape():
-    model = pspnet(input_shape=(720, 960, 3), num_classes=11)
+    model = PSPNet(input_shape=(720, 960, 3), num_classes=11)
+    print(model.summary())
     assert model is not None, "pspnet should compile properly"
 
 def test_pspnet_invalid_shape():
     try:
-        model = pspnet(input_shape=(39, 39, 3), num_classes=11)
+        model = PSPNet(input_shape=(39, 39, 3), num_classes=11)
     except AssertionError:
         assert True, "should have failed input shape assertion"
+
+def test_fpn_valid_shape():
+    model = FPN(input_shape=(288, 288, 3), num_classes=11)
+    print(model.summary())
+    assert model is not None, "fpn should compile properly"
