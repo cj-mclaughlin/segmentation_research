06641d73af71e3c64ee671536f361a33d3411665
cj-mclaughlin <cjmclaughlin@wpi.edu>
2021-07-19 23:26:14 -0400

    fix package setup and regularizer bugs

--- segmentation_research/utils/regularizers.py
+++ segmentation_research/utils/regularizers.py
@@ -8,8 +8,8 @@ def weight_standardization(kernel):
     """
     kernel_mean = tf.math.reduce_mean(kernel, axis=[0, 1, 2], keepdims=True, name='kernel_mean')
     kernel = kernel - kernel_mean
-    kernel_std = tf.keras.backend.std(kernel, axis=[0, 1, 2], keepdims=True, name='kernel_std')
+    kernel_std = tf.math.reduce_std(kernel, axis=[0, 1, 2], keepdims=True, name='kernel_std')
     kernel = kernel / (kernel_std + 1e-5)
 
-WEIGHT_DECAY = l2(1e-5)
+WEIGHT_DECAY = l2(1e-4)
 WS_STD = weight_standardization
