1b26d4118fb0ae79d253507a8ad2c0adaba1f5c8
Connor McLaughlin <46900583+cj-mclaughlin@users.noreply.github.com>
2021-07-23 14:03:35 -0400

    refactor conv->norm into block (#5)

--- segmentation_research/utils/blocks.py
+++ segmentation_research/utils/blocks.py
@@ -1,16 +1,46 @@
 from tensorflow.keras.layers import Conv2D, DepthwiseConv2D, Activation, add
 import tensorflow.keras.backend as K
 from .normalization import get_norm_layer
-from .regularizers import WEIGHT_DECAY, WS_STD
+from .regularizers import WEIGHT_DECAY
 
+def Conv2dNorm(
+    filters,
+    kernel_size=(3,3),
+    strides=(1, 1),
+    padding='same',
+    dilation_rate=(1, 1),
+    kernel_initializer='he_normal',
+    kernel_regularizer=WEIGHT_DECAY,
+    normalization=None):
+
+    def wrapper(input):
+        x = Conv2D(
+            filters, 
+            kernel_size, 
+            padding=padding, 
+            strides=strides, 
+            dilation_rate=dilation_rate, 
+            kernel_regularizer=kernel_regularizer, 
+            kernel_initializer=kernel_initializer,
+            use_bias=not normalization
+        )(input)
+        if normalization is not None:
+            x = get_norm_layer(normalization, filters)(x)
+        return x
+
+    return wrapper
 
 def conv_norm_act(input, filters, dilation_rate=1, strides=1, kernel_size=(3,3), normalization='batchnorm', regularizer=WEIGHT_DECAY, activation='relu'):
     """
     conv -> normalization -> activation block
     """
-    x = Conv2D(filters, kernel_size, padding="same", strides=strides, dilation_rate=dilation_rate, kernel_regularizer=regularizer, use_bias=not normalization)(input)
-    if normalization is not None:
-        x = get_norm_layer(normalization, filters)(x)
+    x = Conv2dNorm(
+        filters, 
+        kernel_size, 
+        strides=strides, 
+        dilation_rate=dilation_rate, 
+        kernel_regularizer=regularizer,
+        normalization=normalization)(input)
     x = Activation(activation)(x)
     return x
 
@@ -23,14 +53,22 @@ def residual_block(input, filters, dilation_rate=1, strides=1, normalization='ba
     if K.int_shape(input)[-1] != filters or strides != 1:
         shortcut = Conv2D(filters, kernel_size=(1,1), strides=strides, kernel_regularizer=regularizer)(shortcut)
     # conv 1
-    x1 = Conv2D(filters, kernel_size=(3,3), strides=1, padding="same", dilation_rate=dilation_rate, use_bias=not normalization, kernel_regularizer=regularizer)(input)
-    if normalization is not None: 
-        x1 = get_norm_layer(normalization, filters)(x1)
+    x1 = Conv2dNorm(
+        filters, 
+        kernel_size=(3,3), 
+        strides=1, 
+        dilation_rate=dilation_rate, 
+        kernel_regularizer=regularizer,
+        normalization=normalization)(input)
     x1 = Activation(activation)(x1)
     # conv 2
-    x2 = Conv2D(filters, kernel_size=(3,3), strides=strides, padding="same", dilation_rate=dilation_rate, use_bias=not normalization, kernel_regularizer=regularizer)(x1)
-    if normalization is not None: 
-        x2 = get_norm_layer(normalization, filters)(x2)
+    x2 = Conv2dNorm(
+        filters, 
+        kernel_size=(3,3), 
+        strides=strides, 
+        dilation_rate=dilation_rate, 
+        kernel_regularizer=regularizer,
+        normalization=normalization)(x1)
     # residual
     residual = add([shortcut, x2])
     residual = Activation(activation)(residual)
@@ -45,19 +83,29 @@ def bottleneck_block(input, filters, dilation_rate=1, strides=1, normalization='
     if K.int_shape(input)[-1] != filters*4 or strides != 1:
         shortcut = Conv2D(filters*4, kernel_size=(1,1), strides=strides, kernel_regularizer=regularizer)(shortcut)
     # conv 1
-    x1 = Conv2D(filters, kernel_size=(1,1), strides=1, padding="same", dilation_rate=1, use_bias=not normalization, kernel_regularizer=regularizer)(input)
-    if normalization is not None: 
-        x1 = get_norm_layer(normalization, filters)(x1)
+    x1 = Conv2dNorm(
+        filters, 
+        kernel_size=(1,1), 
+        strides=1,
+        kernel_regularizer=regularizer,
+        normalization=normalization)(input)
     x1 = Activation(activation)(x1)
     # conv 2
-    x2 = Conv2D(filters, kernel_size=(3,3), strides=1, padding="same", dilation_rate=dilation_rate, use_bias=not normalization, kernel_regularizer=regularizer)(x1)
-    if normalization is not None: 
-        x2 = get_norm_layer(normalization, filters)(x2)
+    x2 = Conv2dNorm(
+        filters, 
+        kernel_size=(3,3), 
+        strides=1, 
+        dilation_rate=dilation_rate, 
+        kernel_regularizer=regularizer,
+        normalization=normalization)(x1)
     x2 = Activation(activation)(x2)
     # conv 3
-    x3 = Conv2D(filters*4, kernel_size=(1, 1), strides=strides, padding="same", dilation_rate=dilation_rate, use_bias=not normalization, kernel_regularizer=regularizer)(x2)
-    if normalization is not None: 
-        x3 = get_norm_layer(normalization, filters)(x3)
+    x3 = Conv2dNorm(
+        filters*4, 
+        kernel_size=(1,1), 
+        strides=strides,
+        kernel_regularizer=regularizer,
+        normalization=normalization)(x2)
     # residual
     residual = add([shortcut, x3])
     residual = Activation(activation)(residual)
@@ -74,19 +122,30 @@ def inverted_bottleneck_block(input, filters, dilation_rate=1, strides=1, normal
     if K.int_shape(input)[-1] != filters or strides != 1:
         shortcut = Conv2D(filters, kernel_size=(1,1), strides=strides, kernel_regularizer=regularizer)(shortcut)
     # conv 1
-    x1 = Conv2D(expansion * in_channels, kernel_size=(1,1), strides=1, padding="same", dilation_rate=1, use_bias=not normalization, kernel_regularizer=regularizer)(input)
-    if normalization is not None: 
-        x1 = get_norm_layer(normalization, expansion * in_channels)(x1)
+    x1 = Conv2dNorm(
+        filters=expansion * in_channels, 
+        kernel_size=(1,1), 
+        strides=1,
+        kernel_regularizer=regularizer,
+        normalization=normalization)(input)
     x1 = Activation(activation)(x1)
     # conv 2
-    x2 = DepthwiseConv2D(kernel_size=(3,3), strides=strides, padding="same", dilation_rate=dilation_rate, use_bias=not normalization, kernel_regularizer=regularizer)(x1)
+    x2 = DepthwiseConv2D(
+        kernel_size=(3,3), 
+        strides=strides, 
+        dilation_rate=dilation_rate, 
+        use_bias=not normalization, 
+        kernel_regularizer=regularizer)(x1)
     if normalization is not None: 
         x2 = get_norm_layer(normalization, expansion * in_channels)(x2)
     x2 = Activation(activation)(x2)
     # conv 3
-    x3 = Conv2D(filters, kernel_size=(1,1), strides=1, padding="same", use_bias=not normalization, kernel_regularizer=regularizer)(x2)
-    if normalization is not None: 
-        x3 = get_norm_layer(normalization, filters)(x3)
+    x3 = Conv2dNorm(
+        filters, 
+        kernel_size=(1,1), 
+        strides=1,
+        kernel_regularizer=regularizer,
+        normalization=normalization)(x2)
     # residual
     residual = add([shortcut, x3])
     return residual
