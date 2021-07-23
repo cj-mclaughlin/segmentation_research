300e8c7e70b69d8bdc62c148dcfe4d60ed3e3739
cj-mclaughlin <cjmclaughlin@wpi.edu>
2021-07-23 11:26:28 -0400

    refactor drn architecture

--- tests/test_backbones.py
+++ tests/test_backbones.py
@@ -1,8 +1,23 @@
 from segmentation_research.backbones.drn import drn, drn_b_26, drn_c_26, drn_c_105
 from tensorflow.keras.layers import Input
-
+from tensorflow.keras.models import Model
 
 INPUT = Input((None, None, 3))
+
+def count_layers(backbone, layer_name):
+    count = 0
+    features = backbone(INPUT)[-1]
+    model = Model(INPUT, features)
+    for layer in model.layers:
+        if layer_name in layer.name:
+            count += 1
+    return count
+
+def test_architecture():
+    resid_b = count_layers(drn_b_26, "add")
+    resid_c = count_layers(drn_c_26, "add")
+    assert resid_b > resid_c, "b architecture should have more residual connections"
+
 
 def test_drn_prebuilts():
     for m in [drn_b_26, drn_c_26, drn_c_105]:
@@ -14,3 +29,5 @@ def test_drn_invalid_arch():
         model = drn(INPUT, arch="D")
     except ValueError:
         assert True, "should have arch assertion"
+
+test_architecture()
